58f5df3357a490681579ca92a0768efa0f9ced19
“genesisoliva” <gjoliva@email.neit.edu>
2021-10-05 11:26:39 -0400

    test

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<h2>SE 266 Class Examples</h2>
-<ul>
-    <li><a href="week1/index.php">Week 1</a></li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>SE 266 Class</h2>
+    <ul>
+        <li>
+            <a href="week1/Mini-Task C Basic PHP and Arrays/index.view.php">Week 1</a>
+        </li>
+    </ul>
+</body>
+</html>
