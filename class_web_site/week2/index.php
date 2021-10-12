0cbb1e2ec83bc68c2387344cd41d70c1020b2534
Oliva <005501789@N358-B4.local>
2021-10-12 16:15:16 -0400

    update

--- class_web_site/week2/index.php
+++ class_web_site/week2/index.php
@@ -2,19 +2,10 @@
 
     <h1>Week 2 </h1>
         <li>
-            <a href="Mini-Task C Basic PHP and Arrays/"></a>
+            <a href="Wk2: Task A: Main Course Page/">Wk2: Task A: Main Course Page</a>
         </li>
         <li>
-            <a href="Mini-Task D Associative Arrays/">Mini-Task D Associative Arrays</a>
-        </li>
-        <li>
-            <a href="Mini-Task E Booleans and Conditionals/">Mini-Task E Booleans and Conditionals</a>
-        </li>
-        <li>
-            <a href="Mini-Task F Functions/">Mini-Task F Functions</a>
-        </li>
-        <li>
-            <a href="Mini-Task G Fizz Buzz/">Mini-Task G Fizz Buzz</a>
+            <a href="Wk2: Task B: Patient Intake Form/">Wk2: Task B: Patient Intake Form</a>
         </li>
 
 <?php include __DIR__ . '/../include/footer.php'; ?>
