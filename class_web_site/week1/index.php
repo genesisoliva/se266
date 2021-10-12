d7d65f6ec230285d3a0dd4997addb7d2e6949dab
Oliva <005501789@N358-B4.local>
2021-10-12 15:55:31 -0400

    update

--- class_web_site/week1/index.php
+++ class_web_site/week1/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php include __DIR__ . '/../include/header.php'; ?>
+
     <h1>Week 1 </h1>
         <li>
             <a href="Mini-Task C Basic PHP and Arrays/">Mini-Task C Basic PHP and Arrays</a>
@@ -23,5 +16,5 @@
         <li>
             <a href="Mini-Task G Fizz Buzz/">Mini-Task G Fizz Buzz</a>
         </li>
-</body>
-</html>
+
+<?php include __DIR__ . '/../include/footer.php'; ?>
