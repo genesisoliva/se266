213095dff15b67b0c55c17842393a3ac3b497f68
Oliva <005501789@N358-B4.local>
2021-10-12 15:50:22 -0400

    update

--- class_web_site/week1/Mini-Task C Basic PHP and Arrays/index.view.php
+++ class_web_site/week1/Mini-Task C Basic PHP and Arrays/index.view.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php include __DIR__ . '/../include/header.php'; ?>
     <h1>Basic PHP and Arrays</h1>
     <ul>
         <?php
@@ -14,5 +6,4 @@
             echo '<li>' . $animal . "</li>";
         ?> 
     </ul>
-</body>
-</html>
+<?php include __DIR__ . '/../include/footer.php'; ?>
