ca3f3a8e14caca381e614b8c5d15eaeb700d3334
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-10-19 14:44:30 -0400

    Update index.php

--- class_web_site/week2/index.php
+++ class_web_site/week2/index.php
@@ -2,9 +2,6 @@
 
     <h1>Week 2 </h1>
         <li>
-            <a href="">Wk2: Task A: Main Course Page</a>
-        </li>
-        <li>
             <a href="01forms.php">Wk2: Task B: Patient Intake Form</a>
         </li>
 
