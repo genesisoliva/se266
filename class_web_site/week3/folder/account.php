2e7433879c04d4a7c222758806bb0c05aea96727
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-10-21 22:20:15 -0400

    Update account.php

--- class_web_site/week3/folder/account.php
+++ class_web_site/week3/folder/account.php
@@ -3,52 +3,38 @@
         protected $accountId, $balance, $startDate;
         
         public function __construct ($id, $b, $sd) {
-           
-            // write code here
-            $this->accountId = $id;
-            $this->balance = $b;
-            $this->startDate = $sd;
-
+           $this->accountId = $id;
+           $this->balance = $b;
+           $this->startDate = $sd;
         }
         public function deposit ($amount) {
-            
             // write code here
-            return $this->balance = $this->balance + $amount;
-
+            $this->balance += $amount;
         }
 
         abstract public function withdrawal($amount);
         // this is an abstract method. This method must be defined in all classes
         // that inherit from this class
         public function getStartDate() {
-
-            // write code here
             return $this->startDate;
         }
 
         public function getBalance() {
-
-            // write code here
             return $this->balance;
         }
 
         public function getAccountId() {
-
-            // write code here
             return $this->accountId;
-
         }
 
         protected function getAccountDetails() {
             // populate $str with the account details
+            $str = "";
+            $str .= "Account ID:" . " "  . $this->accountId . "<br>";
+            $str .= "Balance:" . " "  . $this->balance . "<br>";
+            $str .= "Account Opened:" . " "  . $this->startDate . "<br>";
             
-            $str = "<b>Account ID:</b> " . self::getAccountId() . "<br><b>Balance:</b> " . self::getBalance() . "<br><b>Start Date:</b> " . self::getStartDate();
             return $str;
-        }
-
-        public function setBalance($b){
-
-            $this->balance = $b;
         }
     }
 
@@ -56,22 +42,23 @@
         const OVERDRAW_LIMIT = -200;
 
         public function withdrawal($amount) {
-            // write code here. Return true if withdrawal goes through; false otherwise
-            if(parent::getBalance() - $amount < self::OVERDRAW_LIMIT){
-
-                return false;
-
-            } else {
-
-                parent::setBalance(parent::getBalance() - $amount);
-                return true;
+            // write code here. Return true if withdrawal goes through; false otherwise           
+            
+            if($this->balance - $amount > self::OVERDRAW_LIMIT){
+                $withdraw = true;
+                $this->balance-=$amount; 
             }
+            else{
+                $withdraw = false;                
+            }
+            
+            return $withdraw;
         }
 
         //freebie. I am giving you this code.
         public function getAccountDetails() {
-            //$str = "<h2>Checking Account</h2>";
-            $str = parent::getAccountDetails();
+            $str = "<h2>Checking Account</h2>";
+            $str .= parent::getAccountDetails();
             
             return $str;
         }
@@ -81,36 +68,47 @@
 
         public function withdrawal($amount) {
             // write code here. Return true if withdrawal goes through; false otherwise
-            if(parent::getBalance() - $amount < 0){
-
-                return false;
-
-            } else {
-
-                parent::deposit(-$amount);
-                return true;
-
+            if($this->balance - $amount > 0){
+                $withdraw = true;
+                $this->balance-=$amount; 
             }
+            else{
+                $withdraw = false;                
+            }
+            
+            return $withdraw;
         }
 
         public function getAccountDetails() {
-           // look at how it's defined in other class. You should be able to figure this out ...
-           //$str = "<h2>Savings Account</h2>";
-           $str = parent::getAccountDetails();
-
-           return $str;
+            $str = "<h2>Savings Account</h2>";
+            $str .= parent::getAccountDetails();
+            
+            return $str;
         }
     }
-
     
     
-    //$checking->withdrawal(200);
-    //$checking->deposit(500);
-
-
+/*
+    $checking = new CheckingAccount ('C123', 1000, '12-20-2019');
+    $savings = new SavingsAccount('S123', 5000, '03-20-2020');
+    $checkingBalance = $savingBalance =
+    $checkingAccountID =  $checkingStartDate = $savingAccountID = $savingStartDate = "";
+*/
     
-    //echo $checking->getAccountDetails();
-    //echo $savings->getAccountDetails();
-    //echo $checking->getStartDate();
+
+
+
+
+
+    /* Testing purpose ################
+    $checking->withdrawal(200);
+    $checking->deposit(500);
+    
+    $savings->deposit(500);
+    $savings->withdrawal(2000);
+    
+    echo $checking->getAccountDetails();
+    echo $savings->getAccountDetails(); */
+    
     
 ?>
