6a91ec6ed874e67e0092560000873100a8ab5d23
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-10-21 20:56:43 -0400

    Update index.php

--- class_web_site/week3/index.php
+++ class_web_site/week3/index.php
@@ -2,7 +2,7 @@
 
     <h1>Week 3</h1>
         <li>
-            <a href="">Wk3 Task: Simulate an ATM</a>
+            <a href="atm.php">Wk3 Task: Simulate an ATM</a>
         </li>
 
 <?php include __DIR__ . '/../include/footer.php'; ?>
