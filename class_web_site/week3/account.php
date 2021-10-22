280adaf9cdbf14428e13b18584e85830cdc03d9a
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-10-21 23:05:29 -0400

    Update account.php

--- class_web_site/week3/account.php
+++ class_web_site/week3/account.php
@@ -1,49 +1,54 @@
 <?php
-
-error_reporting(E_ALL ^ E_WARNING); 
-
     abstract class Account {
-        
         protected $accountId, $balance, $startDate;
         
         public function __construct ($id, $b, $sd) {
-            //write code here
-           $this->accountId = $id;
-           $this->balance = $b;
-           $this->startDate = $sd;
-        }
+           
+            // write code here
+            $this->accountId = $id;
+            $this->balance = $b;
+            $this->startDate = $sd;
 
+        }
         public function deposit ($amount) {
-            //write code here
-            $this->balance = $this->balance + $amount;
+            
+            // write code here
+            return $this->balance = $this->balance + $amount;
+
         }
 
         abstract public function withdrawal($amount);
-        //This is an abstract methoth
-           
+        // this is an abstract method. This method must be defined in all classes
+        // that inherit from this class
         public function getStartDate() {
-            //write code here
+
+            // write code here
             return $this->startDate;
         }
 
         public function getBalance() {
-             //write code here
+
+            // write code here
             return $this->balance;
         }
 
         public function getAccountId() {
-             //write code here
+
+            // write code here
             return $this->accountId;
+
         }
 
         protected function getAccountDetails() {
-            //populate
-            $str = " ";
-            $str .= $this->getAccountId();
-            $str .= $this->getBalance();
-            $str .= $this->getStartDate();
-
+            // populate $str with the account details
+            
+            $str = "<b>Account ID:</b> " . self::getAccountId() . "<br><b>Balance:</b> " . self::getBalance() . "<br><b>Start Date:</b> " . self::getStartDate();
             return $str;
+        }
+
+        public function setBalance($b){
+
+            $this->balance = $b;
         }
     }
 
@@ -51,21 +56,22 @@ error_reporting(E_ALL ^ E_WARNING);
         const OVERDRAW_LIMIT = -200;
 
         public function withdrawal($amount) {
-             //write code here
-            if (($this->balance - $amount) >= self::OVERDRAW_LIMIT){
+            // write code here. Return true if withdrawal goes through; false otherwise
+            if(parent::getBalance() - $amount < self::OVERDRAW_LIMIT){
 
-                $this->balance = $this->balance - $amount;
+                return false;
 
-            }else{
+            } else {
 
-                echo "Error: Exceeded withdrawl amount limit.";
-             
+                parent::setBalance(parent::getBalance() - $amount);
+                return true;
             }
         }
-          
+
+        //freebie. I am giving you this code.
         public function getAccountDetails() {
-            $str = "<h2>Checking Account</h2>";
-            $str .= parent::getAccountDetails();
+            //$str = "<h2>Checking Account</h2>";
+            $str = parent::getAccountDetails();
             
             return $str;
         }
@@ -74,29 +80,37 @@ error_reporting(E_ALL ^ E_WARNING);
     class SavingsAccount extends Account {
 
         public function withdrawal($amount) {
-            
-            if (($this->balance - $amount) >= 0){
-                
-                $this->balance = $this->balance - $amount;
+            // write code here. Return true if withdrawal goes through; false otherwise
+            if(parent::getBalance() - $amount < 0){
 
-            }else{
+                return false;
 
-                echo "Error: Exceeded withdrawl amount limit.";
-             
+            } else {
+
+                parent::deposit(-$amount);
+                return true;
+
             }
         }
 
         public function getAccountDetails() {
-          
-           $str = "<h2>Savings Account</h2>";
-           $str .= parent::getAccountDetails();
+           // look at how it's defined in other class. You should be able to figure this out ...
+           //$str = "<h2>Savings Account</h2>";
+           $str = parent::getAccountDetails();
 
-           
            return $str;
-            
         }
     }
 
+    
+    
+    //$checking->withdrawal(200);
+    //$checking->deposit(500);
 
+
+    
+    //echo $checking->getAccountDetails();
+    //echo $savings->getAccountDetails();
+    //echo $checking->getStartDate();
     
 ?>
