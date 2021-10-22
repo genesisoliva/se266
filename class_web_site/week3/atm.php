9ce50162a93dda8bce67eb495eca7c842efe2841
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-10-21 21:11:36 -0400

    Update atm.php

--- class_web_site/week3/atm.php
+++ class_web_site/week3/atm.php
@@ -35,3 +35,63 @@ require 'account.php';
 
 ?>
     
+<div class="container">  
+
+<form method="post" style="text-align:center;">
+
+<h1>ATM</h1>
+       
+
+   <div class="wrapper">
+           
+           <div class="atmAccount" style="border: 1px solid black;
+            padding: 10px; margin:10px;">
+          
+
+           <h2>Checking Account</h2>
+           <li>Account ID: C123</li>
+           <li>Balance: $<?= $newcheckingvar ?></li>
+           <li>Account Opened: 12-20-2019</li> 
+
+                  <input type="hidden" id="test" name="checkingAccountId" value="C123" />
+                  <input type="hidden" name="checkingDate" value="12-20-2019" />
+                  <input type="hidden" name="checkingBalance" value="<?=$newcheckingvar?>" />
+             
+                   <div class="accountInner">
+                       <input type="text" name="checkingWithdrawAmount" value="" />
+                       <input type="submit" class="accBtns" name="withdrawChecking" value="Withdraw" />
+                   </div>
+                   <div class="accountInner">
+                       <input type="text" name="checkingDepositAmount" value="" />
+                       <input type="submit" class="accBtns" name="depositChecking" value="Deposit" /><br />
+                   </div>
+           
+           </div>
+
+           <div class="atmAccount" style="border: 1px solid black;
+            padding: 10px; margin:10px;">
+           
+           
+           <h2>Savings Account</h2>
+           <li>Account ID: S123</li>
+           <li>Balance: $<?= $newsavingvar ?></li>
+           <li>Account Opened: 03-20-2020</li>  
+
+                <input type="hidden" name="savingsAccountId" value="S123" />
+                <input type="hidden" name="savingsDate" value="03-20-2020" />
+                <input type="hidden" name="savingsBalance" value="<?=$newsavingvar?>" />
+              
+                   <div class="accountInner">
+                       <input type="text" name="savingsWithdrawAmount" value="" />
+                       <input type="submit" class="accBtns" name="withdrawSavings" value="Withdraw" /><br />
+                   </div>
+                   <div class="accountInner">
+                       <input type="text" name="savingsDepositAmount" value="" />
+                       <input type="submit" class="accBtns" name="depositSavings" value="Deposit" /><br />
+                           
+                   
+                   </div>
+           
+           </div>
+           
+       </div>
