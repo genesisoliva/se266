fbb1992496433f66473ee45cc0aec2a0b07bd836
Oliva <gjoliva@labs.neit.edu>
2021-10-26 15:32:34 -0400

    update

--- class_web_site/week4/index.php
+++ class_web_site/week4/index.php
@@ -1,5 +1,8 @@
 <?php include __DIR__ . '/../include/header.php'; ?>
 
     <h1>Week 4</h1>
+    <li>
+            <a href="/teams/view.php">View</a>
+    </li>
 
 <?php include __DIR__ . '/../include/footer.php'; ?>
