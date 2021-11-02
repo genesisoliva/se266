e8df8a826a609a0528bd6f70293ea0b024642c7c
Oliva <005501789@N358-B5.local>
2021-11-02 14:20:13 -0400

    update

--- class_web_site/week4/teams/model/db.php
+++ class_web_site/week4/teams/model/db.php
@@ -1,9 +1,8 @@
-
 <?php
 
 $ini = parse_ini_file( __DIR__ . '/dbconfig.ini');
 
-$db = new PDO(  "mysql:host=" . $ini['server'] . 
+$db = new PDO(  "mysql:host=" . $ini['servername'] . 
                 ";port=" . $ini['port'] . 
                 ";dbname=" . $ini['dbname'], 
                 $ini['username'], 
@@ -13,6 +12,3 @@ $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
-
-
-
