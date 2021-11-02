0620dcda10f645b46299cdcc8f45277ca25151ca
Oliva <005501789@N358-B5.local>
2021-11-02 18:08:06 -0400

    updaye

--- class_web_site/include/header.php
+++ class_web_site/include/header.php
@@ -310,18 +310,18 @@ svg:not(:root).svg-inline--fa {
 <script>
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropDown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+//function dropDown() {
+  //document.getElementById("myDropdown").classList.toggle("show");
+//}
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
+//window.onclick = function(e) {
+ // if (!e.target.matches('.dropbtn')) {
+  //var myDropdown = document.getElementById("myDropdown");
+   // if (myDropdown.classList.contains('show')) {
+   //   myDropdown.classList.remove('show');
+   // }
+  //}
+//}
 </script>
 
 <div class="nav nav-sky">
