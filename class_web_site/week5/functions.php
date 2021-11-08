dd487010f369a54baae51dde8cb528483090fb83
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-08 12:36:01 -0500

    Delete functions.php

--- class_web_site/week5/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function isPostRequest() {
-    return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
-}
-function calcAge($birthday)
-    {
-        $today = new DateTime('today');
-        return date_diff(date_create($birthday), $today);
-    }
