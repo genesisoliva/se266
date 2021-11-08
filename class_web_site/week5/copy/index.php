5b2ff676a49f6b0af32f352060112428eda3ac69
Oliva <005501789@N358-A5.local>
2021-11-08 12:50:12 -0500

    update

--- class_web_site/week5/copy/index.php
+++
@@ -1,8 +0,0 @@
-<?php include __DIR__ . '/../include/header.php'; ?>
-
-    <h1>Week 5</h1>
-    <li>
-            <a href="viewPatients.php">View</a>
-    </li>
-
-<?php include __DIR__ . '/../include/footer.php'; ?>
