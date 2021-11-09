ab80f4f75fa9de31986ebc55d8c65996efdcd7a3
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-08 23:56:04 -0500

    Update functions.php

--- php-magic-linking/assignments/week_6/school_reference_database/includes/functions.php
+++ php-magic-linking/assignments/week_6/school_reference_database/includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-includes_once __DIR__ '/../models/model_schools.php';
+include_once __DIR__ '/../models/model_schools.php';
     function isPostRequest() {
         return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
     }
