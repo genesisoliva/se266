4050c5e7ba26ff29a39b19088d41650672d14803
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-09 02:07:35 -0500

    Update index.php

--- php-magic-linking/assignments/week_6/school_reference_database/index.php
+++ php-magic-linking/assignments/week_6/school_reference_database/index.php
@@ -6,9 +6,23 @@
     session_start();
     include_once __DIR__ . "/models/model_schools.php";
 
-    if (isPostRequest()) {
-       
-}
+    if (isPostRequest())
+            {
+                if (isset($_POST['uname']) && isset($_POST['pword']) && $_POST['uname'] != "" && $_POST['pword'] != "") {
+                    $result = "";
+                    $user = IsValidUser(filter_input(INPUT_POST, 'uname'), sha1(filter_input(INPUT_POST, 'pword')));
+                    if ($user == true) {
+                        $_SESSION["User"] = filter_input(INPUT_POST, 'uname');
+                        header('Location: upload.php');
+                    } else {
+                        $result = "Invalid username or password";
+                    }
+                } else {
+                    $result = "Please enter a username and password";
+                }
+            }
+        
+        ?>
 ?>
 <form method="post" action="index.php">
     <h3>Please Login</h3>
@@ -23,6 +37,8 @@
             </div>
             
         </form>
+
+<br /><center><h3><?php if (isPostRequest()) {echo $result;} ?></h3></center>
 
 
 	</main>
