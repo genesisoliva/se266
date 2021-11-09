cab311f517931ae566bd458e040ecd6d56908ad5
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-09 02:03:24 -0500

    Update index.php

--- php-magic-linking/assignments/week_6/school_reference_database/index.php
+++ php-magic-linking/assignments/week_6/school_reference_database/index.php
@@ -5,7 +5,6 @@
     error_reporting(E_ALL ^ E_WARNING);
     session_start();
     include_once __DIR__ . "/models/model_schools.php";
-    include_once __DIR__ . "/includes/functions.php";
 
     if (isPostRequest()) {
        
