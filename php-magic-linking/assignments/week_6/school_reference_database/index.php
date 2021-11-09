8a7461c7e277473275148bcc4c7c1b6c3b0c78dc
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-09 01:57:00 -0500

    Update index.php

--- php-magic-linking/assignments/week_6/school_reference_database/index.php
+++ php-magic-linking/assignments/week_6/school_reference_database/index.php
@@ -16,10 +16,10 @@
 <form method="post" action="index.php">
     <h3>Please Login</h3>
             <div class="rowContainer">
-            User Name:<input type="text" name="uname" value="<?=$username="donald";?>">
+            User Name:<input type="text" name="uname" value="donald">
             </div>
             <div class="rowContainer">
-                Password:<input type="password" name="pword" value="<?=$password="duck";?>">
+                Password:<input type="password" name="pword" value="duck">
             </div>
               <div class="rowContainer">
                   <input type="submit" name="login" value="Login" class="btn btn-warning">
