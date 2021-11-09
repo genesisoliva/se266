0df0298e5868acbdff560ef52f80b2caed068c3d
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-09 00:38:39 -0500

    Delete php-magic-linking/assignments/week_6/school_reference_database/new directory

--- php-magic-linking/assignments/week_6/school_reference_database/new/includes/footer.php
+++
@@ -1,5 +0,0 @@
-</div>
-
-<div class="footer">&copy;2021 by Genesis Oliva</div>
-</body>
-</html>
--- php-magic-linking/assignments/week_6/school_reference_database/new/includes/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-    function isPostRequest() {
-        return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
-    }
