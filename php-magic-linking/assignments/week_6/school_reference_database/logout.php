98b9b6fe816cbc9d8996bb9ab42dd200f54b7de8
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-10 04:46:35 -0500

    Update logout.php

--- php-magic-linking/assignments/week_6/school_reference_database/logout.php
+++ php-magic-linking/assignments/week_6/school_reference_database/logout.php
@@ -3,25 +3,11 @@
 
 <?php
     // delete the session info and redirect to login.php
-    /*session_start();
+    session_start();
     session_unset();
     session_destroy();
 
-    header('Location: index.php');*/
-
-session_start();
-    if(filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST')
-    {
-        if(isset($_POST['logoff'])) 
-        {
-            $_SESSION['loggedIn'] = "no";
-        }
-    }
-    if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == "no")
-    {
-        session_destroy();
-        header("Location: http://se266-sherry-j-2020.herokuapp.com/");
-    }
+    header('Location: index.php');
 
 ?>
 
