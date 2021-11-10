74a83a5469ff7acacde55ec2bdee4632ac7b9667
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-10 05:21:07 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,43 +1,75 @@
 <?php
-if(isset($_POST['submit'])){ 
-    $dbHost = "localhost";        //Location Of Database usually its localhost 
-    $dbUser = "xxxx";            //Database User Name 
-    $dbPass = "xxxxxx";            //Database Password 
-    $dbDatabase = "db_name";    //Database Name 
-     
-    $db = mysql_connect($dbHost,$dbUser,$dbPass)or die("Error connecting to database."); 
-    //Connect to the databasse 
-    mysql_select_db($dbDatabase, $db)or die("Couldn't select the database."); 
-    //Selects the database 
-     
-    /* 
-    The Above code can be in a different file, then you can place include'filename.php'; instead. 
-    */ 
-     
-    //Lets search the databse for the user name and password 
-    //Choose some sort of password encryption, I choose sha256 
-    //Password function (Not In all versions of MySQL). 
-    $usr = mysql_real_escape_string($_POST['username']); 
-    $pas = hash('sha256', mysql_real_escape_string($_POST['password'])); 
-    $sql = mysql_query("SELECT * FROM users_table  
-        WHERE username='$usr' AND 
-        password='$pas' 
-        LIMIT 1"); 
-    if(mysql_num_rows($sql) == 1){ 
-        $row = mysql_fetch_array($sql); 
-        session_start(); 
-        $_SESSION['username'] = $row['username']; 
-        $_SESSION['fname'] = $row['first_name']; 
-        $_SESSION['lname'] = $row['last_name']; 
-        $_SESSION['logged'] = TRUE; 
-        header("Location: page3.php"); // Modify to go to the page you would like 
-        exit; 
-    }else{ 
-        header("Location: page1.php"); 
-        exit; 
-    } 
-}else{    //If the form button wasn't submitted go to the index page, or login page 
-    header("Location: index.php");     
-    exit; 
-} 
-?> 
+
+  session_start(); /* Starts the session */
+
+  if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in */
+    header("location:page1.php");
+	  exit;
+  }
+?>
+
+<!-- Show password protected content down here -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>Logged in</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">PHP Login by @mariofont</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Status: logged in</h1>
+        <p class="lead">And just like that you've created your first password protected area with PHP and a little knowledge of HTML. Change the username and password in login.php for your own and give it a try!</p>
+        <p><a class="btn btn-lg btn-success" href="logout.php" role="button">Log out</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; Mario Font 2016</p>
+      </footer>
+
+    </div>
+
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
