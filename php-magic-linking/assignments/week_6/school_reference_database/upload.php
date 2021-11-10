bf74e3a86504a27f195efe153a6560ee55fd1c84
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-10 04:06:11 -0500

    Update upload.php

--- php-magic-linking/assignments/week_6/school_reference_database/upload.php
+++ php-magic-linking/assignments/week_6/school_reference_database/upload.php
@@ -8,19 +8,18 @@
     
     if (isset ($_FILES['file1'])) {
         // upload the file to uploads folder and then call insertSchoolsFromFile 
-        if (isset ($_FILES['file1'])) {
 
             $tmp_name = $_FILES['file1']['tmp_name'];
             $path = getcwd() .DIRECTORY_SEPARATOR . 'uploads';
             $new_name = $path . DIRECTORY_SEPARATOR . $_FILES['file1']['name'];
             move_uploaded_file($temp_name, $new_name);
             insertSchoolsFromFile($new_name);
+
+//redirect to search.php
+
             header('Location: search.php');
             exit; 
         } 
-        //redirect to search.php
-    echo "File uploaded";
-    header('Location: search.php');
 }
 
     include_once __DIR__ . "/header.php";
