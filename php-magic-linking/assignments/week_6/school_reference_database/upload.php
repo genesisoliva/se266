0bf96be0379bfe731a868270c32fb4a271a3fd87
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-10 04:18:43 -0500

    Update upload.php

--- php-magic-linking/assignments/week_6/school_reference_database/upload.php
+++ php-magic-linking/assignments/week_6/school_reference_database/upload.php
@@ -2,9 +2,9 @@
 	    <h1 class="cent"><?php echo $pageTitle ?></h1>
 <?php
     include_once __DIR__ . "/models/model_schools.php";
-    include_once __DIR__ . "/includes/functions.php";
+    //include_once __DIR__ . "/includes/functions.php";
 
-    session_start();
+    //session_start();
     
     if (isset ($_FILES['file1'])) {
         // upload the file to uploads folder and then call insertSchoolsFromFile 
@@ -18,7 +18,7 @@
 //redirect to search.php
 
             header('Location: search.php');
-            exit; 
+            //exit; 
         } 
 }
 
@@ -33,6 +33,11 @@
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="file1">
         <input type="submit" value="Upload">
+
+<?php
+                    if(isset($_FILES['file1']))
+                        echo "<div id = 'font-family:itimFont;'>File Uploaded!</div>";
+                ?>
 
     </form>    
 
