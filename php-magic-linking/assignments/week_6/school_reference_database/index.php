2d1756e350b9ddfd014e7f1feaf488478b214017
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-10 04:13:32 -0500

    Update index.php

--- php-magic-linking/assignments/week_6/school_reference_database/index.php
+++ php-magic-linking/assignments/week_6/school_reference_database/index.php
@@ -4,10 +4,10 @@
     <?php
 
     include_once __DIR__ . "/models/model_schools.php";
-    include_once __DIR__ . "/includes/functions.php";
+    //include_once __DIR__ . "/includes/functions.php";
     session_start();
 
-if (isPostRequest()) {
+/*if (isPostRequest()) {
         $username = filter_input(INPUT_POST, 'username');
         $password = filter_input(INPUT_POST, 'password');
 
@@ -21,6 +21,15 @@ if (isPostRequest()) {
             echo "Please enter in a valid username and password.";
 
         }
+    }*/
+
+session_start();
+    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST')
+        if (checkLogin($_POST['username'],$_POST['password']))
+            $_SESSION['loggedIn'] = "yes";
+    if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == "yes") {
+        deleteAllSchools();
+        header("Location: upload.php");
     }
 
     
