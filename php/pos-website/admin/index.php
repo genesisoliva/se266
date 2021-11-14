895f867c77d74dba6c81c74464ecfbec4843527c
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-14 15:17:37 -0500

    Create index.php

---
+++ php/pos-website/admin/index.php
@@ -0,0 +1,36 @@
+<?php //include('session.php');
+session_start();
+	if (!isset($_SESSION['id']) ||(trim ($_SESSION['id']) == '')) {
+	header('location:../index.php');
+    exit();
+	}
+	
+	include('../conn.php');
+
+	$sq=mysqli_query($conn,"select * from `user` where userid='".$_SESSION['id']."'");
+	$srow=mysqli_fetch_array($sq);
+	
+	$user=$srow['username'];
+?>
+<?php include('header.php'); ?>
+<body>
+<div id="wrapper">
+<?php include('navbar.php'); ?>
+<div style="height:50px;"></div>
+<div id="page-wrapper">
+<div class="container-fluid">
+<div style="height:40px;"></div>
+	<img src="../upload/cover.jpg" class="thumbnail" style="height:430px; width:1030px;">
+</div>
+</div>
+</div>
+<?php include('script.php'); ?>
+<?php include('modal.php'); ?>
+</body>
+<div class="foot">
+<footer>
+<p> THIS PROJECT IS BROUGHT TO YOU BY <a href="https://code-projects.org/">CODE-PROJECTS</a> || www.code-projects.org ||</p>
+</footer>
+<style> .foot{text-align: center; border: 2px solid black;}</style>
+</div>
+</html>
