1f385dd5f1cb519689d67490a1088897c2252f2c
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-15 20:51:21 -0500

    Update db.sql

--- class_web_site/week7/db.sql
+++ class_web_site/week7/db.sql
@@ -1,8 +1,18 @@
 CREATE TABLE IF NOT EXISTS resturant (
-id VARCHAR2 UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY,
-address VARCHAR2 DEFAULT NULL,
-city VARCHAR2 DEFAULT NULL,
-state CHAR(2) DEFAULT NULL,
-zipcode VARCHAR2,
-phonenumber CHAR(10)
+resturant_id VARCHAR UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY,
+resturant_address VARCHAR DEFAULT NULL,
+resturant_city VARCHAR DEFAULT NULL,
+resturant_state CHAR(2) DEFAULT NULL,
+resturant_zipcode VARCHAR,
+resturant_phonenumber CHAR(10)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS employee (
+resturant_id VARCHAR(10) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY,
+resturant_fullname VARCHAR(20) DEFAULT NULL,
+resturant_payrate NUMBER(2,2) DEFAULT NULL,
+resturant_role CHAR(2) DEFAULT NULL,
+resturant_zipcode VARCHAR,
+resturant_phonenumber CHAR(10)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
