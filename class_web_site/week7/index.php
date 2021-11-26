5fc6f2e62ea06dfcd9b5609199f500962805a0d2
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-26 15:08:28 -0500

    Update index.php

--- class_web_site/week7/index.php
+++ class_web_site/week7/index.php
@@ -5,6 +5,6 @@
             <a href="login.php">Patient Search</a>
     </li>
     <li>
-            <a href="/Proposal/index.php">🧰 Final Project: Proposal</a>
+            <a href="/index.php">🧰 Final Project: Proposal</a>
     </li>
 <?php include __DIR__ . '/../include/footer.php'; ?>
