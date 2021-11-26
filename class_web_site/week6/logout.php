17865e242d51d989c25fb851e7cdbe94a9bfc8ef
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-26 15:01:36 -0500

    Update logout.php

--- class_web_site/week6/logout.php
+++ class_web_site/week6/logout.php
@@ -1,9 +1,5 @@
 <?php 
 include_once __DIR__ . "/models/model_schools.php";
-?>
-	    <h1 class="cent"><?php echo $pageTitle ?></h1>
-
-<?php
     // delete the session info and redirect to login.php
     deleteAllSchools ();
     session_start();
