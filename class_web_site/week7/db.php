cbb8a23d2be16544ed331a5cae49e58036b8ceb3
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-26 15:26:29 -0500

    Update db.php

--- class_web_site/week7/db.php
+++ class_web_site/week7/db.php
@@ -11,4 +11,27 @@ $db = new PDO(  "mysql:host=" . $ini['server'] .
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+class Connection {
+	/*********************************************************************************************
+	 * DATABASE CONNECTION
+     *********************************************************************************************/
+	public static function conn() {
+		$server = "ict.neit.edu";
+		$port = "5500";
+		$dbname = "se266_genesis";
+		$dsn = "mysql:host=$server;port=$port;dbname=$dbname";
+		$username = "se266_genesis";
+		$password = "genesis";
+		try {
+			$conn = new PDO($dsn, $username, $password);
+			$conn->exec('set names utf8');
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $conn;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+	}
+}
+
 ?>
