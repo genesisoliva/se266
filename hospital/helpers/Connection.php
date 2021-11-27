d5bcd83b16800c4bf150881f4e6c4549ade3b39c
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-26 22:48:23 -0500

    Update Connection.php

--- hospital/helpers/Connection.php
+++ hospital/helpers/Connection.php
@@ -5,12 +5,12 @@ class Connection {
 	 * DATABASE CONNECTION
      *********************************************************************************************/
 	public static function conn() {
-		$host = "localhost";
-		$port = "3306";
-		$dbname = "hospital";
+		$host="ict.neit.edu";//$host = "localhost";
+		$port=5500;//$port = "3306";
+		$dbname="se266_genesis";//$dbname = "hospital";
 		$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
-		$username = "alansary";
-		$password = "123456";
+		$username="se266_genesis";//$username = "alansary";
+		$password="genesis";//$password = "123456";
 		try {
 			$conn = new PDO($dsn, $username, $password);
 			$conn->exec('set names utf8');
