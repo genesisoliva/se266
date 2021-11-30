2d531fd40988594c804e0ee3045d95365d730014
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-29 23:55:33 -0500

    Update functions.php

--- class_web_site/week7/includes/functions.php
+++ class_web_site/week7/includes/functions.php
@@ -3,6 +3,11 @@
     function isPostRequest() {
         return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
     }
+	
+	function calcAge($birthday) {
+        $today = new DateTime('today');
+        return date_diff(date_create($birthday), $today);
+    }
 
   if(isset($_GET['id']))
 {
@@ -66,4 +71,4 @@ else
 
     }
 
-}
+}?>
