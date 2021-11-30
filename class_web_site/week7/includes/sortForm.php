fa2bc61614d6a6324c25bcb134ac9375d2ef5894
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-29 23:58:25 -0500

    Update sortForm.php

--- class_web_site/week7/includes/sortForm.php
+++ class_web_site/week7/includes/sortForm.php
@@ -1,13 +1,17 @@
 <br>
 <form  method="post">
-    <input type="" name="action" value="sort"> <br>
+    <input type="hidden" name="action" value="sort"> <br>
        <label>Sort By Field:</label>
        <select name="fieldName">
-              <option value="">Select One</option>
-              <option value="teamName">Team Name</option>
-              <option value="division">Division</option>
+            <option value=""></option>
+			<option value="id">Patient #</option>
+      <option value="patientFirstName">First Name</option>
+<option value="patientMiddleName">Middle Name</option>
+	  <option value="patientLastName">Last Name</option>
+	  <option value="patientBirthDate">Date of Birth</option>
+	  <option value="patientMarried">Married</option>
           </select>
-       <input type="radio" name="fieldValue" value="ASC" checked />Ascending
+       <input type="radio" name="fieldValue" value="ASC" />Ascending
        <input type="radio" name="fieldValue" value="DESC" />Descending
        
       <button type="submit"  name="sortTeam">Sort</button>
