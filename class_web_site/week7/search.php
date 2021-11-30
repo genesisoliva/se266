3e11f99727a5732e57439be0d165d6cd3f88e740
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-29 23:52:24 -0500

    Update search.php

--- class_web_site/week7/search.php
+++ class_web_site/week7/search.php
@@ -1,102 +1,59 @@
 <?php
-    include_once __DIR__ . "/models/model_patients.php";
-    include_once __DIR__ . "/includes/functions.php";
-   
-    session_start();
-
+session_start();
     if($_SESSION['Login'] == false)
     {
         header("location: login.php");
 	    exit;
     }
-
-    $firstName = "";
-    $lastName = "";
-    $married = "";
-    
-    $patientResults = [];
-
-    if($_SESSION['Login'] == false)
-    {
-        header("location: login.php");
-	    exit;
-    }
-
-    if (isPostRequest()) {
-
-        $firstName = filter_input(INPUT_POST, 'firstName');
-        $lastName = filter_input(INPUT_POST, 'lastName');
-        $married = filter_input(INPUT_POST, 'married');
-    
-        $patientResults = getPatientsSearch($firstName, $lastName, $married);
-
+	include __DIR__ . '/models/model_patients.php';
+	include __DIR__ . '/includes/functions.php';
+  
+	if (isPostRequest()) {
         $id = filter_input(INPUT_POST, 'patientID');
         deletePatient ($id);
          
-    }  
-
-    include_once __DIR__ . "/includes/header.php";
+    }
+    $action = filter_input(INPUT_POST, 'action');
+    $fieldName = filter_input(INPUT_POST, 'fieldName');
+    $fieldValue = filter_input(INPUT_POST, 'fieldValue');
+    if ( $action === 'sort' && $fieldName != '' ) {
+            $patients = sortPatients ($fieldName, $fieldValue);   
+    }
+    else if ( $action === 'search' && $fieldName != '' ) {
+            $patients = searchPatients ($fieldName, $fieldValue);
+    } else {
+        $patients = getPatients();  
+    }
 ?>
 
-<h2>Patient Search</h2>
-            <form method="post" action="search.php">
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+            <meta charset="UTF-8">
+    <style type="text/css">
+        body {margin-left: 20px;}
+    </style>
+        <title>Search and Sort</title>
+    </head>
+    <body>
+	
+        <?php
+		include_once __DIR__ . "/includes/header.php";
+		
+		include __DIR__ . '/includes/searchForm.php';
+            include __DIR__ . '/includes/sortForm.php';
+            include __DIR__ . '/includes/Results.php';
+			
+			?>
 
-                <div class="rowContainer">
-                   <label class="col1">First Name:</label>
-                   <div class="col2"><input type="text" name="firstName" value="<?php echo $firstName; ?>"></div> 
-               </div>
 
-               <div class="rowContainer">
-                   <label class="col1">Last Name:</label>
-                   <div class="col2"><input type="text" name="lastName" value="<?php echo $lastName; ?>"></div> 
-               </div>
+            <a href="patient.php?action=add" id="addBtn">Add Patient</a>
 
-               <div class="rowContainer">
-                   <label class="col1">Married:</label>
-                   <div class="col2">
-                        <input type="radio" name="married" value="1" <?=$married==1?"":"";?>><label>Yes</label>
-                        <input type="radio" name="married" value="0" <?=$married==0?"":"";?>><label>No</label>
-                    </div> 
-               </div>
-
-                 <div class="rowContainer">
-                   <div class="col1">&nbsp;</div>
-                   <div class="col2"><input type="submit" name="search" value="Search" class="btn btn-warning"></div> 
-               </div>
-            </form>
-            
-            <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Marital Status</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-            <?php foreach ($patientResults as $row): ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['patientFirstName']; ?></td>
-                    <td><?php echo $row['patientLastName']; ?></td>
-                    <td><?= $row['patientMarried']==1?"Yes":"No";?></td>
-                    <td><a href="edit.php?id=<?=$row['id']?>&action=update" style="display:contents;">Edit</a></td>
-
-                    <td>
-                        <form action="search.php" method="post">
-                            <input type="hidden" name="patientID" value="<?php echo $row['id'];?>">
-                            <button class="btn glyphicon glyphicon-trash" type="submit" style="display:contents;"></button>
-                        </form>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-
-              
-            </tbody>
-        </table>
-
-        <a href="edit.php?action=add" id="addBtn">Add Patient</a>
+		<?php
+        include_once __DIR__ . "/includes/footer.php";
+        ?>
+    </body>
+</html>
