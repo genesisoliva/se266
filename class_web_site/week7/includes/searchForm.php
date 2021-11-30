a508879e68290f229dc558571e8265fffb87da80
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-29 19:45:33 -0500

    Update searchForm.php

--- class_web_site/week7/includes/searchForm.php
+++ class_web_site/week7/includes/searchForm.php
@@ -1,15 +1,19 @@
 
    <h2>Search Team</h2>
   <form method="post">
-      <input type="hidden" name="action" value="search" />
+      <input type="" name="action" value="search" /><br>
       <label>Search by Field:</label>
        <select name="fieldName">
               <option value="">Select One</option>
               <option value="teamName">Team Name</option>
               <option value="division">Division</option>
-              
           </select>
+
+<br>
+
        <input type="text" name="fieldValue" value= "<?= $fieldValue ?>">
-      <button type="submit">Search</button>
+      <br>
+<button type="submit">Search</button>
+
       
   </form>
