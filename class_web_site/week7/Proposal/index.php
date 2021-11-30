258391c7febb4a3401a0332ebcae368e1b729be6
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-30 00:01:44 -0500

    Delete class_web_site/week7/Proposal directory

--- class_web_site/week7/Proposal/index.php
+++
@@ -1,7 +0,0 @@
-<?php include __DIR__ . '/../../include/header.php'; ?>
-
-<h1>Final Project: Proposal</h1>
-<li>
-            <a href="../Proposal/login.php">login</a>
-</li>
-<?php include __DIR__ . '/../../include/footer.php'; ?>
