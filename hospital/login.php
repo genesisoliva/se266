7bc7e1b5de6dc4520ccebd4e3730959a33322314
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-11-30 21:08:16 -0500

    Update login.php

--- hospital/login.php
+++ hospital/login.php
@@ -164,7 +164,7 @@
 ?>
 
 <!DOCTYPE html>
-<?php if ($_SESSION['lang']) { ?>
+<?php if ($_SESSION['lang']) == 1{ ?>
 <html lang="ar" dir="rtl">
 <?php } elseif ($_SESSION['lang']) == 2{ ?>
 <html lang="es" dir="ltr">
