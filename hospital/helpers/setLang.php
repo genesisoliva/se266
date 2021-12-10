b5623be8e2fb0eb9006f9dac3489cb7dd2b14bd1
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-12-10 12:03:55 -0500

    Update setLang.php

--- hospital/helpers/setLang.php
+++ hospital/helpers/setLang.php
@@ -1,33 +1,16 @@
 <?php
-	if (isset($_GET['lang'])) 
-	{
-		if ($_GET['lang'] == 'en') 
-		{
-			$_SESSION['lang'] = 0;
-			$selectedLang = "en";
-		}
-        elseif($_GET['lang'] == 'es') 
-		{
-			$_SESSION['lang'] = 2;
-			$selectedLang = "es";
-		}
-		elseif($_GET['lang'] == 'pt')
-		{
-			$_SESSION['lang'] = 3;
-			$selectedLang = "pt";
-		}
 
-
-
-		
-		else 
-		{
-			$_SESSION['lang'] = 1;
-			$selectedLang = "ar";
-		}
-	}
-	else 
-	{
+if (isset($_GET['lang'])) {
+	if ($_GET['lang'] == 'en') {
 		$_SESSION['lang'] = 0;
 		$selectedLang = "en";
 	}
+	else {
+		$_SESSION['lang'] = 1;
+		$selectedLang = "ar";
+	}
+}
+else {
+	$_SESSION['lang'] = 0;
+	$selectedLang = "en";
+}
