29c8edc7d2175d4022bff0cc770f73bccec91abc
Genesis <42148976+genesisoliva@users.noreply.github.com>
2021-12-15 21:35:57 -0500

    Update index.php

--- class_web_site/week8/index.php
+++ class_web_site/week8/index.php
@@ -1,5 +1,8 @@
 <?php include __DIR__ . '/../include/header.php'; ?>
 
     <h1>Week 8 </h1>
+<li>
+            <a href="login.php">Disney Votes</a>
+    </li>
 
 <?php include __DIR__ . '/../include/footer.php'; ?>
